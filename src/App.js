98478470eec2404eb307b05b5a6f70904f427de7
bishoybishai <bishoy.bishai@gmail.com>
2019-09-22 23:17:30 +0200

    Initiate React project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+const App = () => {
+  return (
+      <div>Secret Escape</div>
+  );
+};
+
+render(<App />, document.getElementById('app-root'));
